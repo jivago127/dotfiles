e442f32f7208634dcf4ab9e601dd513b8206a38e
Jivago <jivago.127@gmail.com>
2026-03-31 10:59:54 -0300

    alacritty e update nix configs

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -1,36 +1,25 @@
 # Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
+# your system. Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-	<home-manager/nixos>
-    ];
+  imports = [ 
+    ./hardware-configuration.nix
+    <home-manager/nixos>
+  ];
 
-  # Bootloader.
+  # Bootloader settings
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
+  networking.hostName = "nixos";
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
+  # Localization and Time
   time.timeZone = "America/Araguaina";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "pt_BR.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "pt_BR.UTF-8";
     LC_IDENTIFICATION = "pt_BR.UTF-8";
@@ -43,26 +32,22 @@
     LC_TIME = "pt_BR.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the Budgie Desktop environment.
-  services.xserver.displayManager.lightdm.enable = true;
-  services.xserver.desktopManager.budgie.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "br";
-    variant = "";
+  # Graphics and Desktop Environment (Budgie)
+  services.xserver = {
+    enable = true;
+    displayManager.lightdm.enable = true;
+    desktopManager.budgie.enable = true;
+    xkb = {
+      layout = "br";
+      variant = "";
+    };
   };
 
-  # Configure console keymap
+  # Keyboard layout for the console (TTY)
   console.keyMap = "br-abnt2";
 
-  # Enable CUPS to print documents.
+  # Sound and Printing
   services.printing.enable = true;
-
-  # Enable sound with pipewire.
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -70,92 +55,60 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # Package and User Management
+  nixpkgs.config.allowUnfree = true;
   
-  programs.zsh.enable = true;
-
-users.users.jivago = {
-    shell = pkgs.zsh;
+  users.users.jivago = {
     isNormalUser = true;
+    shell = pkgs.zsh;
     description = "Jivago";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
-    #  thunderbird
+      # Specific user packages can go here or in home.nix
     ];
   };
 
-
-home-manager.users.jivago = import ./home.nix;
-
-
-#################################################################
-#       Configs para todos os usuários.
-#################################################################
-  users.defaultUserShell = pkgs.zsh;
-  # Install firefox.
-  programs.firefox.enable = true;
-  programs.zsh.ohMyZsh = {
-	enable = true;
-	theme = "agnoster";
+  # Home Manager Configuration
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    users.jivago = {
+      imports = [ ./home.nix ];
+      # This allows unfree for home-manager specifically
+      nixpkgs.config.allowUnfree = true;
+    };
   };
-  
 
+  # Shell and Global Programs
+  users.defaultUserShell = pkgs.zsh;
+  programs.zsh = {
+    enable = true;
+    ohMyZsh = {
+      enable = true;
+      theme = "agnoster";
+    };
+  };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  programs.firefox.enable = true;
+  # System-wide packages
   environment.systemPackages = with pkgs; [
-  	vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  	wget
-	zsh
-	git
-	alacritty
-	gh
+    	vim
+    	wget
+    	git
+    	alacritty
+    	gh
+	maven
   ];
 
+  # Fonts (Required for the 'agnoster' Zsh theme)
   fonts.packages = with pkgs; [
-	nerd-fonts.fira-code
-	nerd-fonts.sauce-code-pro
+    nerd-fonts.fira-code
+    nerd-fonts.sauce-code-pro
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.11"; # Did you read the comment?
-
+  # This value determines the NixOS release version. 
+  # Note: 25.11 suggests you are tracking the unstable branch.
+  system.stateVersion = "25.11"; 
 }
--- nix/home.nix
+++ nix/home.nix
@@ -2,54 +2,85 @@
 
 {
   # Versão do Home Manager (ajuste conforme seu sistema)
-  home.stateVersion = "25.11"; 
+  home.stateVersion = "25.11";
 
   # Pacotes específicos do seu usuário
   home.packages = with pkgs; [
-    andromeda-gtk-theme
-    papirus-icon-theme
-    temurin-bin-25 
+    	andromeda-gtk-theme
+    	papirus-icon-theme
+   	temurin-bin-25
+	nixpkgs-fmt
+	maven
+	htop
   ];
 
   # Configuração do Zsh e Oh My Zsh
   programs.zsh = {
-    enable = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
+  	enable = true;
+	autosuggestion.enable = true;
+   	syntaxHighlighting.enable = true;
+    
+	oh-my-zsh = {
+      		enable = true;
+      		plugins = [ "git" ];
+      		theme = "agnoster"; # Sinta-se à vontade para mudar
+    	};
+};
 
-    oh-my-zsh = {
-      enable = true;
-      # plugins = [ "git" ];
-      theme = "agnoster"; # Sinta-se à vontade para mudar
-    };
-  };
-	# A identação tá ficando uma droga
-	programs.gh.enable = true;
-	programs.gh.gitCredentialHelper.enable = true;
+# A identação tá ficando uma droga
+programs.gh.enable = true;
+programs.gh.gitCredentialHelper.enable = true;
 
-  # Configuração do Tema GTK (Andromeda)
-  gtk = {
-    enable = true;
-    theme = {
-      name = "Andromeda";
-      package = pkgs.andromeda-gtk-theme;
-    };
-    iconTheme = {
-      name = "Papirus-Dark";
-      package = pkgs.papirus-icon-theme;
-    };
-  };
+# Configuração do Tema GTK (Andromeda)
+gtk = {
+    	enable = true;
+    	theme = {
+      		name = "Andromeda";
+      		package = pkgs.andromeda-gtk-theme;
+	};
 
-  # Cursor (Opcional, mas combina com o Andromeda)
-  home.pointerCursor = {
-    package = pkgs.bibata-cursors;
-    name = "Bibata-Modern-Classic";
-    size = 24;
-    gtk.enable = true;
-  };
+  	iconTheme = {
+      		name = "Papirus-Dark";
+      		package = pkgs.papirus-icon-theme;
+ 	 };
+};
 
- programs.java = {
+
+
+# Cursor (Opcional, mas combina com o Andromeda)
+
+home.pointerCursor = {
+	package = pkgs.bibata-cursors;
+    	name = "Bibata-Modern-Classic";
+    	size = 24;
+    	gtk.enable = true;
+};
+
+programs.java = {
  	enable = true;
 	package = pkgs.temurin-bin-25;
- };
+};
+
+programs.vscode = {
+	enable = true;
+	package = pkgs.vscode;
+
+	userSettings = {
+		"java.jdt.ls.java.home" = "${pkgs.temurin-bin-25}";
+		"java.configuration.runtimes" = [
+			{
+			name = "JavaSE-25";
+			"path" = "${pkgs.temurin-bin-25}";
+			"default" = true;
+			}
+		];
+		"java.configuration.maven.userSettings"
+			 = "${config.home.homeDirectory}/m2/settins.xml";
+		"maven.executable.path" = "${pkgs.maven}/bin/mvn";
+		"java.import.maven.wrapper.enabled" = false;
+	};
+};
+
+
+
 }
